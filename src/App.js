8fc008792ce9398f8c50b21a9d927ff413dc6b4a
Abdelwahab <abdelwahabsaeed415@gmail.com>
2025-05-17 23:45:46 +0300

    use context

--- src/App.js
+++ src/App.js
@@ -6,18 +6,25 @@ import Register from './pages/Register';
 import Movies from './pages/Movies';
 import MovieDetails from './pages/MovieDetails';
 import Favourites from './pages/Favourites';
+import { LanguageContext } from './Context/Lang';
+import { useState } from 'react';
 function App() {
+
+  const [myContextLang, setMyContextLang] = useState('en');
+
   return (
     <div className="App">
       <BrowserRouter>
-        <Switch>
-          <Route exact path="/" component={Movies} />
-          <Route exact path="/login" component={Login} />
-          <Route exact path="/register" component={Register} />
-          <Route exact path="/movies" component={Movies} />
-          <Route exact path="/movies/:movieId" component={MovieDetails} />
-          <Route exact path="/favourites" component={Favourites} />
-        </Switch>
+        <LanguageContext.Provider value={{myContextLang, setMyContextLang}}>
+          <Switch>
+            <Route exact path="/" component={Movies} />
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/movies" component={Movies} />
+            <Route exact path="/movies/:movieId" component={MovieDetails} />
+            <Route exact path="/favourites" component={Favourites} />
+          </Switch>
+        </LanguageContext.Provider>
       </BrowserRouter>
     </div>
   );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,12 @@
 import { Link } from 'react-router-dom';
 import { FaHeart, FaRegHeart } from 'react-icons/fa';
 import { useDispatch, useSelector } from "react-redux"
-import { use } from 'react';
+import { useContext } from 'react';
+import { LanguageContext } from '../Context/Lang';
 
 export default function Navbar() {
+
+  const {myContextLang, setMyContextLang} = useContext(LanguageContext);
 
   const movies = useSelector((state) => state.favourites.favourites);
   console.log(movies.length);
@@ -25,6 +28,13 @@ export default function Navbar() {
                 </div>
             </div>
             <div className="d-flex align-items-center justify-content-end gap-3">
+                  <div className="d-flex align-items-center justify-content-end gap-3">
+                    <h2>{ myContextLang}</h2>
+                    <select className="form-select text-white" value={myContextLang} onChange={(e) => setMyContextLang(e.target.value)}>
+                      <option value="en" >English</option>
+                      <option value="ar">Arabic</option>
+                    </select>
+                  </div>
                   <Link className="nav-link text-white" to="/login">Login</Link>
                   <Link className="nav-link text-white" to="/register">Register</Link>
                   <Link className="nav-link text-white" to="/favourites">Favourites({movies.length})</Link>
